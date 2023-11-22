daa94d567194954fabdce703a2a43cc4b6373c48
root <root@Desenv-Joo.softsite.private>
2023-11-22 12:11:32 -0300

    script to run gsolad

---
+++ hestia/gsolad_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ./.env
+
+docker run -p 8091:8091 --add-host=pumyra:10.0.0.82 -e BASE_USERNAME=$BASE_USERNAME -e BASE_PASSWORD=$BASE_PASSWORD -e HOST_BASE=$HOST_BASE registry.gitlab.com/softsite/geosales/gsolad:2023.10.25
