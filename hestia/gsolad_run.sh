91982d63196da5e201422790546fb8ac60820dbd
root <root@Desenv-Joo.softsite.private>
2023-11-22 16:56:55 -0300

    gsolad script run hello world - mid step

--- hestia/gsolad_run.sh
+++ hestia/gsolad_run.sh
@@ -2,4 +2,5 @@
 
 source ./.env
 
-docker run -p 8091:8091 --add-host=pumyra:10.0.0.82 -e BASE_USERNAME=$BASE_USERNAME -e BASE_PASSWORD=$BASE_PASSWORD -e HOST_BASE=$HOST_BASE registry.gitlab.com/softsite/geosales/gsolad:2023.10.25
+docker run -v /var/run/docker.sock:/var/run/docker.sock hestia -- run hello-world
+#docker run -p 8091:8091 --add-host=pumyra:10.0.0.82 -e BASE_USERNAME=$BASE_USERNAME -e BASE_PASSWORD=$BASE_PASSWORD -e HOST_BASE=$HOST_BASE registry.gitlab.com/softsite/geosales/gsolad:2023.10.25
