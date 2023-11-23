1b6a49269f2165a37e98eb6e2c5e9934b19e899e
root <root@Desenv-Joo.softsite.private>
2023-11-22 21:35:50 -0300

    debug rm registry login

--- hestia/debug.sh
+++ hestia/debug.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-source ./.env
-
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)/gitlab_registry_login.sh:/gitlab_registry_login.sh" -v "$(pwd)/.env:/app/.env" -it --entrypoint sh hestia:latest -c "/gitlab_registry_login.sh $LOGIN $PASSWORD && sh"
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)/gitlab_registry_login.sh:/gitlab_registry_login.sh" -v "$(pwd)/.env:/app/.env" -it --entrypoint sh hestia:latest 
